cca2dde5236bde46c0eea775bb61feb916d6e5cb
Muri <mcsanchezlaguna@gmail.com>
2024-09-24 18:25:26 +0200

    Added "SectionFooteJuansTrainer" component

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Navbar from "../components/Navbar";
-
+import SectionFooteJuansTrainer from "../SectionFooterJuansTrainer";
 import SectionFooterMuri from "../SectionFooterMuri";
 
 export default function PageHome() {
@@ -9,6 +9,7 @@ export default function PageHome() {
     <div className="PageHome">
       <Navbar />
       <h1>Contenido de la página de home</h1>
+      <SectionFooteJuansTrainer />
       <SectionFooterMuri />
     </div>
   );
