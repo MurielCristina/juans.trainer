b1853395de7733daa7cc5e13372cba039d9d2c4e
Muri <mcsanchezlaguna@gmail.com>
2024-09-25 16:37:00 +0200

    working on footer

--- src/components/FooterJuansTrainer.js
+++ src/components/FooterJuansTrainer.js
@@ -1,15 +1,14 @@
 import React from "react";
-import "./FooterJuansTrainer.css";
 
+import "./FooterJuansTrainer.css";
 import logoDark from "../images/logoDark.jpeg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function FooteJuansTrainer() {
   return (
-    <div className=".dartk-theme FooterJuansTrainer">
+    <div className=" FooterJuansTrainer">
       <div className="row">
         <div className="col-4">
           <img
@@ -20,36 +19,27 @@ export default function FooteJuansTrainer() {
         </div>
         <div className="col-4">
           <h4>Located in:</h4>
-          <a href="https://maps.app.goo.gl/73TzVcxoha9gXb7e7">
+          <a
+            href="https://maps.app.goo.gl/73TzVcxoha9gXb7e7"
+            target="_blank"
+            rel="noreferrer"
+          >
             📍 Oporto, Portugal
           </a>
-          <h4>Follow me on:</h4>
-          <FontAwesomeIcon icon={faLinkedinIn} className="footer-icons" />
-          <FontAwesomeIcon icon={faInstagram} className="footer-icons" />
-          <h4>Contact me on:</h4>
-          <FontAwesomeIcon icon={faEnvelope} className="footer-icons" />
         </div>
-        <div className="col-2">
-          <h4 className="text-center">
-            <small>Workout programs</small>
-          </h4>
-          <ul>
-            <li>xxxxx</li>
-            <li>xxxxx</li>
-            <li>xxxxx</li>
-            <li>xxxxx</li>
-          </ul>
-        </div>
-        <div className="col-2">
-          <h4 className="text-center">
-            <small>More</small>
-          </h4>
-          <ul>
-            <li>xxxxx</li>
-            <li>xxxxx</li>
-            <li>xxxxx</li>
-            <li>xxxxx</li>
-          </ul>
+        <div className="col-4">
+          <h5>Follow me on:</h5>
+          <a
+            href="https://www.ºinstagram.com/juans.trainer/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} className="footer-icons" />
+          </a>
+          <h5>Contact me on:</h5>
+          <a href="mailto:jsanchezlaguna18@gmail.com">
+            jsanchezlaguna18@gmail.com
+          </a>
         </div>
       </div>
       <div></div>
