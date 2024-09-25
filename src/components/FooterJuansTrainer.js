bca0638ff45d4d43194b3eae23d16df9a968284e
Muri <mcsanchezlaguna@gmail.com>
2024-09-25 13:47:02 +0200

    Added font awesome

--- src/components/FooterJuansTrainer.js
+++ src/components/FooterJuansTrainer.js
@@ -3,6 +3,9 @@ import "./FooterJuansTrainer.css";
 
 import logoDark from "../images/logoDark.jpeg";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenNib, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export default function FooteJuansTrainer() {
   return (
     <div className=".dartk-theme FooterJuansTrainer">
@@ -17,6 +20,8 @@ export default function FooteJuansTrainer() {
         <div className="col-4">
           <h4>📍 Oporto, Portugal</h4>
           <h4>Follow me on:</h4>
+          <FontAwesomeIcon icon={faPenNib} />
+          <FontAwesomeIcon icon={faEnvelope} />
         </div>
         <div className="col-4">xxx</div>
       </div>
