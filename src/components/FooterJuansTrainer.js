d1c5b5e3360d2bc6370dce9ecd1a4a4decf5b2cc
Muri <mcsanchezlaguna@gmail.com>
2024-09-25 13:46:53 +0200

    Working on Footer JuansTrainer

--- src/components/FooterJuansTrainer.js
+++ src/components/FooterJuansTrainer.js
@@ -1,18 +1,26 @@
 import React from "react";
-
 import "./FooterJuansTrainer.css";
 
 import logoDark from "../images/logoDark.jpeg";
-import logoLight from "../images/logoLight.jpeg";
 
 export default function FooteJuansTrainer() {
   return (
-    <div className="FooteJuansTrainer">
-      Juans Trainer Footer
-      <div>
-        <img src={logoDark} alt="Juans Trainer logo (dark)" />
-        <img src={logoLight} alt="Muri logo" />
+    <div className=".dartk-theme FooterJuansTrainer">
+      <div className="row">
+        <div className="col-4">
+          <img
+            src={logoDark}
+            alt="Juans Trainer logo (dark)"
+            className="img-fluid rounded-circle"
+          />
+        </div>
+        <div className="col-4">
+          <h4>📍 Oporto, Portugal</h4>
+          <h4>Follow me on:</h4>
+        </div>
+        <div className="col-4">xxx</div>
       </div>
+      <div></div>
     </div>
   );
 }
