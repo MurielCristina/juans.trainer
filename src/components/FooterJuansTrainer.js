2169f13a27ab42976ed0bb43ff86b4a3851e46cf
Muri <mcsanchezlaguna@gmail.com>
2024-09-25 15:12:42 +0200

    styled Footer Juans Trainer

--- src/components/FooterJuansTrainer.js
+++ src/components/FooterJuansTrainer.js
@@ -19,14 +19,38 @@ export default function FooteJuansTrainer() {
           />
         </div>
         <div className="col-4">
-          <h4>📍 Oporto, Portugal</h4>
+          <h4>Located in:</h4>
+          <a href="https://maps.app.goo.gl/73TzVcxoha9gXb7e7">
+            📍 Oporto, Portugal
+          </a>
           <h4>Follow me on:</h4>
-          <FontAwesomeIcon icon={faLinkedinIn} />
-          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faLinkedinIn} className="footer-icons" />
+          <FontAwesomeIcon icon={faInstagram} className="footer-icons" />
           <h4>Contact me on:</h4>
-          <FontAwesomeIcon icon={faEnvelope} />
+          <FontAwesomeIcon icon={faEnvelope} className="footer-icons" />
         </div>
-        <div className="col-4">xxx</div>
+        <div className="col-2">
+          <h4 className="text-center">
+            <small>Workout programs</small>
+          </h4>
+          <ul>
+            <li>xxxxx</li>
+            <li>xxxxx</li>
+            <li>xxxxx</li>
+            <li>xxxxx</li>
+          </ul>
+        </div>
+        <div className="col-2">
+          <h4 className="text-center">
+            <small>More</small>
+          </h4>
+          <ul>
+            <li>xxxxx</li>
+            <li>xxxxx</li>
+            <li>xxxxx</li>
+            <li>xxxxx</li>
+          </ul>
+        </div>
       </div>
       <div></div>
     </div>
