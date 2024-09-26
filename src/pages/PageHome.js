13a43a5133093f60e8d3bf5b9234d52db8ad6778
Muri <mcsanchezlaguna@gmail.com>
2024-09-26 16:06:21 +0200

    hero start done (✅responsive)

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "./PageHome.css";
 
 import Navbar from "../components/Navbar";
+import HeroStart from "./PageHome/HeroStart";
 import FooteJuansTrainer from "../components/FooterJuansTrainer";
 import FooterMuri from "../components/FooterMuri";
 
@@ -8,7 +10,7 @@ export default function PageHome() {
   return (
     <div className="PageHome container">
       <Navbar />
-      <h1>Contenido de la página de home</h1>
+      <HeroStart />
       <FooteJuansTrainer />
       <FooterMuri />
     </div>
---
+++ src/pages/PageHome/HeroStart.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function HeroStart() {
+  return (
+    <section className="HeroStart">
+      <div className="hero-content">
+        <h1>JUANS TRAINER</h1>
+        <h3>Personal Trainer in Oporto, Portugal</h3>
+        <p>
+          Empowering you to achieve your fitness goals with personalized
+          training and dedicated support.
+        </p>
+        <a href="../PageTrainWithMe" class="hero-button btn">
+          Get Started
+        </a>
+      </div>
+    </section>
+  );
+}
