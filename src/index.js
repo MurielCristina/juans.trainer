9ecc63656467b74eb964b566cbd8c586f41ffbd2
Muri <mcsanchezlaguna@gmail.com>
2024-09-27 15:15:18 +0200

    remove routing from index.js and handled it directly in App.js.
    -Cleaner entry point and handle all routing in one place.

--- src/index.js
+++ src/index.js
@@ -2,44 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import PageHome from "./pages/PageHome";
-import PageAbout from "./pages/PageAbout";
-import PagePrograms from "./pages/PagePrograms";
-import PageTrainWithMe from "./pages/PageTrainWithMe";
-
 import "bootstrap/dist/css/bootstrap.css";
-
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <App />,
-  },
-  {
-    path: "/PageHome",
-    element: <PageHome />,
-  },
-  {
-    path: "/PageAbout",
-    element: <PageAbout />,
-  },
-  {
-    path: "/PagePrograms",
-    element: <PagePrograms />,
-  },
-  {
-    path: "/PageTrainWithMe",
-    element: <PageTrainWithMe />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
